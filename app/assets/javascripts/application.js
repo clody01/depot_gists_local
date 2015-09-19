015e14c12fbfba14cabc74fad3cef52be70f5112
Vro Claude <vroclaude@gmail.com>
2015-09-20 01:00:27 +0200

    debug du conflit entre bootstrap et dataTable de jquery

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
+//= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
 //= require bootstrap
 //= require turbolinks
 //= require_tree .
- 
